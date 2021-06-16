d32d8ea4ca5f453f0df8d58af93512825976bd11
Hamza Iqbal <hiqbal@infinitesquare.com>
2021-06-16 03:47:35 +0200

    add loading / error

--- src/app/superheroes-module/pages/superheroes-search-page/superheroes-search-page.component.ts
+++ src/app/superheroes-module/pages/superheroes-search-page/superheroes-search-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ISuperHero } from 'src/app/superheroes-module/models/superhero.model';
 import { SuperHeroesService } from 'src/app/superheroes-module/services/superheros.service';
+import { catchError, finalize, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
 	selector: 'superheroes-search-page',
@@ -10,12 +12,26 @@ export class SuperheroesSearchPageComponent implements OnInit {
 	superHeroes: ISuperHero[] = [];
 	currentSuperHero: ISuperHero;
 
+	isLoading: boolean = true;
+	hasError: boolean = false;
+
+	get isLoaded(): boolean {
+		return this.superHeroes.length > 0;
+	}
+
 	constructor(private superHeroesService: SuperHeroesService) { }
 
 	ngOnInit(): void {
-		this.superHeroesService
-			.getAll()
-			.subscribe(superHeroes => this.superHeroes = superHeroes);
+		this.superHeroesService.getAll()
+			.pipe(
+				tap(superHeroes => this.superHeroes = superHeroes),
+				finalize(() => this.isLoading = false),
+				catchError(e => {
+					this.hasError = true;
+					return of(e);
+				})
+			)
+			.subscribe();
 	}
 
 	onSelectSuperHero(superHero: ISuperHero): void {
