8ba9ad59d511a23e79504b6b3030ccf579d50e42
Hamza Iqbal <hiqbal@infinitesquare.com>
2021-06-18 03:58:39 +0200

    forgot one import

--- src/app/superheroes-module/services/superheros.service.ts
+++ src/app/superheroes-module/services/superheros.service.ts
@@ -1,4 +1,4 @@
-import { environment } from 'src/environments/environment';
+import { environment } from '../../../environments/environment';
 import { ISuperHero } from './../models';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
